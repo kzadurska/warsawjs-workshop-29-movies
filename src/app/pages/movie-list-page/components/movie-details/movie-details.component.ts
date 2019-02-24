73047181b523f148f3edfc6f7f88763a423035e3
Kasia Zadurska <kzadurska@gmail.com>
2019-02-24 17:29:36 +0100

    Display details page

--- src/app/pages/movie-list-page/components/movie-details/movie-details.component.ts
+++ src/app/pages/movie-list-page/components/movie-details/movie-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestService } from 'src/app/request-service.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.component.css']
 })
 export class MovieDetailsComponent implements OnInit {
+  public movie:Object = null // TODO: add interface
 
-  constructor() { }
+  constructor(private requestService: RequestService, private activeRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    const id = this.activeRoute.snapshot.paramMap.get("id")
+    this.requestService.fetchMovie(id).subscribe(response => {
+      console.log(response)
+      return this.movie = response
+    })
   }
+
 
 }
